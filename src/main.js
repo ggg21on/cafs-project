9be5f3b091e37a166960ac74746aff12f5e25a44
Gabriel Gomes Galikosky <gomesgalikosky@gmail.com>
2022-09-13 22:32:55 -0300

    cafs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(createVuestic()).mount('#app')
