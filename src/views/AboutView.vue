7bb8c62089817acc6471faf7394ccd6ad7bb43e3
Gabriel Gomes Galikosky <gomesgalikosky@gmail.com>
2022-09-15 21:55:24 -0300

    cafs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div style="var(--va-secondary);">
+    <va-sidebar hoverable textColor="dark" minimizedWidth="64px">
+      <va-sidebar-item
+        v-for="item in items"
+        :key="item.title"
+        :active="item.active"
+      >
+        <va-sidebar-item-content>
+          <va-icon :name="item.icon" />
+          <va-sidebar-item-title>
+            {{ item.title }}
+          </va-sidebar-item-title>
+        </va-sidebar-item-content>
+      </va-sidebar-item>
+      <va-divider></va-divider>
+    </va-sidebar>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Home", icon: "home" },
+        { title: "Pesquisar", icon: "search" },
+        { title: "Bolsistas", icon: "school" },
+        { title: "Computadores", icon: "computer" },
+        { title: "Projetos", icon: "folder" },
+      ],
+    };
+  },
+};
+</script>
